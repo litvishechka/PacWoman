3f4b5137dd67475ca9d7296b7a1ed57fa7dc9c02
litvishechka <yulia_litvishko@mail.ru>
2023-04-25 23:04:49 +0300

    Add object and change background color.

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,12 +1,19 @@
+#include "utils.cpp"
 #include <windows.h>
-#include <wingdi.h>
 
-bool running = true;
+global_variable bool running = true;
 
-void* buffer_memory;
-int buffer_width;
-int buffer_height;
-BITMAPINFO buffer_bitmap_info;
+struct Render_State {
+    int height, width;
+    void* memory;
+
+    BITMAPINFO bitmap_info;
+};
+
+
+global_variable Render_State render_state;
+
+#include "renderer.cpp"
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     LRESULT result = 0;
@@ -19,19 +26,19 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
         case WM_SIZE: {
             RECT rect;
             GetClientRect(hwnd, &rect); 
-            buffer_width = rect.right - rect.left;
-            buffer_height = rect.bottom - rect.top;   
+            render_state.width = rect.right - rect.left;
+            render_state.height = rect.bottom - rect.top;   
 
-            int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+            int size = render_state.width * render_state.height * sizeof(unsigned int);
 
-            if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
-            buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-            buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
-            buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
-            buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
-            buffer_bitmap_info.bmiHeader.biPlanes = 1;
-            buffer_bitmap_info.bmiHeader.biBitCount = 32;
-            buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+            if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+            render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+            render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+            render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+            render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+            render_state.bitmap_info.bmiHeader.biPlanes = 1;
+            render_state.bitmap_info.bmiHeader.biBitCount = 32;
+            render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
         } break;
 
         default: {
@@ -61,9 +68,11 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
             DispatchMessage(&message);
         }
         //Simulate
+        clear_screen(0xff5500);
+        draw_rect(0, 0, 20, 20, 0x00ff22);
         
         //Render
-        StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+        StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
     }
     
     return 0;
